4d48d40ff25f4462319f0c8a8547ef982be4de87
seongjin77 <kssj9056@gmail.com>
2022-08-01 02:45:24 +0900

    whether get API

--- whethercoalnu/src/App.js
+++ whethercoalnu/src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 // 499d737b2bec38bd9d9551149102200a
 // 현재 위치 기반의 날씨가 보인다.
 // 날씨 정보에는 도시, 섭씨 , 화씨 날씨 상태가 있다.
@@ -8,11 +9,23 @@ import './App.css';
 // 데이터를 들고오는 동안 로딩 스피너가 돈다.
 
 function App() {
-  return (
-    <div>
-      
-    </div>
-  );
+  const getCurrentLocation = () => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      let lat = position.coords.latitude;
+      let lon = position.coords.longitude;
+      getWeatherByCurrentLocation(lat, lon)
+    });
+  };
+
+  const getWeatherByCurrentLocation = async(lat,lon)=> {
+    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=499d737b2bec38bd9d9551149102200a`
+    let response = await fetch(url);
+    let data = await response.json();
+  };
+  useEffect(() => {
+    getCurrentLocation();
+  }, []);
+  return <div></div>;
 }
 
 export default App;

177c93b79f315004a25a906892f0f4267e29e3fa
seongjin77 <kssj9056@gmail.com>
2022-08-07 16:51:11 +0900

    Loading spinner

--- weathercoalnu/src/App.js
+++ weathercoalnu/src/App.js
@@ -1,8 +1,9 @@
 import logo from "./logo.svg";
 import "./App.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import ClipLoader from "react-spinners/ClipLoader";
 import { useEffect, useState } from "react";
-import WeatherBox from './component/WeatherBox';
+import WeatherBox from "./component/WeatherBox";
 import WeatherButton from "./component/WeatherButton";
 // 499d737b2bec38bd9d9551149102200a
 // 현재 위치 기반의 날씨가 보인다.
@@ -12,51 +13,64 @@ import WeatherButton from "./component/WeatherButton";
 // 데이터를 들고오는 동안 로딩 스피너가 돈다.
 
 function App() {
-
   const [weather, setWeather] = useState(null);
-  const [city, setCity] = useState('');
-  const cities = ['paris', 'new york', 'tokyo', 'seoul'];
-  const [cityweather,setCityWeather] = useState(null);
+  const [city, setCity] = useState("");
+  const [loading, setLoading] = useState(false);
+  const cities = ["current Location", "paris", "new york", "tokyo", "seoul"];
+  const [cityweather, setCityWeather] = useState(null);
 
   const getCurrentLocation = () => {
     navigator.geolocation.getCurrentPosition((position) => {
       let lat = position.coords.latitude;
       let lon = position.coords.longitude;
-      getWeatherByCurrentLocation(lat, lon)
-     
+      getWeatherByCurrentLocation(lat, lon);
     });
   };
 
-  const getWeatherByCurrentLocation = async(lat,lon)=> {
-    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=499d737b2bec38bd9d9551149102200a&units=metric`
+  const getWeatherByCurrentLocation = async (lat, lon) => {
+    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=499d737b2bec38bd9d9551149102200a&units=metric`;
+    setLoading(true);
     let response = await fetch(url);
     let data = await response.json();
     setWeather(data);
+    setLoading(false);
   };
 
-  const getWeatherBycity = async()=>{
-    let url =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=499d737b2bec38bd9d9551149102200a&units=metric`
+  const getWeatherBycity = async () => {
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=499d737b2bec38bd9d9551149102200a&units=metric`;
+    setLoading(true);
     let response = await fetch(url);
     let data = await response.json();
     setWeather(data);
-  }
+    setLoading(false);
+  };
 
   useEffect(() => {
-    if(city===''){
+    if (city === "") {
       getCurrentLocation();
-    }else{
+    } else {
       getWeatherBycity();
     }
   }, [city]);
 
-  
-  
-  return <div>
-    <div className="container">
-    <WeatherBox weather={weather}/>
-    <WeatherButton cities={cities} setCity={setCity}/>
+  return (
+    <div>
+      {loading ? (
+        <div className="container">
+          <ClipLoader
+            color="#f88c6b"
+            loading={loading}
+            size={150}
+          />
+        </div>
+      ) : (
+        <div className="container">
+          <WeatherBox weather={weather} />
+          <WeatherButton cities={cities} setCity={setCity} />
+        </div>
+      )}
     </div>
-  </div>;
+  );
 }
 
 export default App;
